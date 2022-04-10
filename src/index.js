ad75460a954d68a380667d212dfd14960b70e51f
chrisyuen976 <chrisyuen976@gmail.com>
2022-04-10 22:17:03 +1000

    build failed. Trying to move folders

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./public/src/components/App";
+import App from "./components/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
