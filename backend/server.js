051679d3187de3d97eead53615c36dfc2c44a80c
chrisyuen976 <chrisyuen976@gmail.com>
2022-04-11 00:10:25 +1000

    deleted proxy links, RES.sendfile?

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('src'));
     app.use(express.static('backend'));
     app.get('/*', (req,res)=>{
-        req.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
     })
 }
 
