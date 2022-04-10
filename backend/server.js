0c702e26ea0354d8a67b761d4ea1b71c89996023
chrisyuen976 <chrisyuen976@gmail.com>
2022-04-10 23:36:53 +1000

    changed Profile

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('public'));
     app.use(express.static('src'));
     app.use(express.static('backend'));
-    app.get('/*', (req,res)=>{
+    app.get('*', (req,res)=>{
         req.sendFile(path.resolve(__dirname, 'public', 'index.html'))
     })
 }
