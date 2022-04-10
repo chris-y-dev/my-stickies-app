a815ae8c8b81793b537793bcad4ceb664d30fe53
chrisyuen976 <chrisyuen976@gmail.com>
2022-04-10 21:02:16 +1000

    changes to if port..., change order of script json

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,23 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const routesURL = require('./routes/routes')
+const routesURL = require('./routes/routes');
+const path = require('path')
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 4000;
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('public'));
+    app.get('*', (req,res)=>{
+        req.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+    })
+}
 
-mongoose.connect(process.env.ATLAS_URI, ()=>{
-    console.log('MongoDB database connected successfully')
-})
+mongoose.connect(process.env.ATLAS_URI, { useUnifiedTopology: true, useNewUrlParser: true })
+    .then(console.log('MongoDB database connected successfully'))
 
 app.use(express.json())
 app.use(cors());
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,3 +4,5 @@ module.exports = function(app){
     app.use(createProxyMiddleware("/note", {target: 'http://localhost:4000'}));
     app.use(createProxyMiddleware("/note/:id", {target: 'http://localhost:4000'}))
 }
+
+//react-scripts start 
