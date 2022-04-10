0115356b5dc8bfacd245786c7ef041420f4fc78a
chrisyuen976 <chrisyuen976@gmail.com>
2022-04-10 11:15:09 +1000

    works locally but not deployed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,7 +14,7 @@ function App() {
   const [noteArray, setNoteArray] = useState([]);
 
   function retrieveData(){
-    fetch("/note")
+    fetch("http://localhost:4000/note")
     .then(res => res.json())
     .then(jsonRes => setNoteArray(jsonRes));
   }
@@ -38,7 +38,7 @@ function App() {
 
     const {keyId, objId} = ids
     // console.log(id);
-    axios.delete('/note/' + objId)
+    axios.delete('http://localhost:4000/note/' + objId)
    .then(res => console.log(res.data));
    //old link http://localhost:4000/
    //testing post instead of delete
