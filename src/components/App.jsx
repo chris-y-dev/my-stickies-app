38fd61f6f1fdb752bc1e5193f3d93713c6b53f92
chrisyuen976 <chrisyuen976@gmail.com>
2022-04-11 01:16:57 +1000

    Web loads HTML but does not render

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,7 +15,7 @@ function App() {
   const [noteArray, setNoteArray] = useState([]);
 
   function retrieveData(){
-    fetch("https://stickies-wall-by-chris.herokuapp.com/note")
+    fetch("/note")
     .then(res => res.json())
     .then(jsonRes => setNoteArray(jsonRes));
   }
@@ -39,7 +39,7 @@ function App() {
 
     const {keyId, objId} = ids
     // console.log(id);
-    axios.delete('https://stickies-wall-by-chris.herokuapp.com/note/' + objId)
+    axios.delete('/note/' + objId)
    .then(res => console.log(res.data));
    //http://localhost:4000/note
    //https://stickies-wall-by-chris.herokuapp.com
