19bb0db8f0a02c6c1e1034d9d49f4cad7cf64bf9
chrisyuen976 <chrisyuen976@gmail.com>
2022-04-11 11:40:14 +1000

    small fixes. Finished!

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 //Function for my footer. 
 function Footer() {
-  const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Created by Chris Yuen {year}</p>
+      <p>Created by Chris Yuen, 2022</p>
       <p><a href="https://www.chrisyuen.info">Portfolio</a> <a href="https://www.linkedin.com/in/chrisyuen976">LinkedIn</a></p>
       <p className="footer-small">Technologies used: React, JavaScript, Node.js, MongoDB</p>
+      <p className="footer-small">Check out my code on <a href="https://github.com/chrisyuen976/my-stickies-app">GitHub</a></p>
     </footer>
   );
 }
